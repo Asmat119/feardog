5fe09ec6065c814c8b36cb0d84591bedf2385e5b
Hazrat Sharif <50118391+hazratsahrif@users.noreply.github.com>
2022-12-18 21:57:51 +0500

    Hazrat done some changes

--- app/src/main/java/com/example/feardog/MainActivity.java
+++ app/src/main/java/com/example/feardog/MainActivity.java
@@ -2,13 +2,22 @@ package com.example.feardog;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
+import android.view.textclassifier.TextClassification;
+import android.widget.TextView;
+
+import com.google.android.material.button.MaterialButton;
 
 public class MainActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        TextView tv =findViewById(R.id.scrollingtext);
+
+        tv.setSelected(true);
     }
 }
