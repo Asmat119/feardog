d952f4ab461b62decd1de61ec706397bee6f8f0a
Sadia <zafarsadia58@gmail.com>
2022-12-19 20:49:11 +0500

    comment done

--- app/src/main/java/com/example/feardog/MainActivity.java
+++ app/src/main/java/com/example/feardog/MainActivity.java
@@ -3,12 +3,27 @@ package com.example.feardog;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.TextView;
+
+import com.example.feardog.databinding.ActivityMainBinding;
+
+import iammert.com.expandablelib.ExpandableLayout;
 
 public class MainActivity extends AppCompatActivity {
+    ActivityMainBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+
+
+
+
+
     }
 }
